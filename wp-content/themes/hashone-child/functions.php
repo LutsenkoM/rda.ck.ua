5bc88f9ad0e5188a3577ee4ce346793f1d1a7165
rda_team <lutsenko.maxim@gmail.com>
2017-02-08 10:59:40 +0200

    [VP] Edited theme 'hashone-child'
    VP-Action: theme/edit/hashone-child
    VP-Theme-Name: hashone-child
    
    X-VP-Version: 4.0-alpha1
    X-VP-Environment: default

--- wp-content/themes/hashone-child/functions.php
+++ wp-content/themes/hashone-child/functions.php
@@ -1,15 +1,15 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Max
- * Date: 08.02.2017
- * Time: 9:49
- */
-
-function load_style_script() {
-    wp_enqueue_style('style.css', get_stylesheet_uri());
-}
-
-add_action('wp_enqueue_scripts', 'load_style_script');
-
-?>
+///**
+// * Created by PhpStorm.
+// * User: Max
+// * Date: 08.02.2017
+// * Time: 9:49
+// */
+//
+//function load_style_script() {
+//    wp_enqueue_style('style.css', get_stylesheet_uri());
+//}
+//
+//add_action('wp_enqueue_scripts', 'load_style_script');
+//
+//?>
