b8dfee006a37085a2386a0751cd2a45a11cf01d7
rda_team <lutsenko.maxim@gmail.com>
2017-02-21 21:31:42 +0200

    [VP] Created option 'sample_theme_options'
    VP-Action: option/create/sample_theme_options
    
    X-VP-Version: 4.0-alpha1
    X-VP-Environment: default

---
+++ wp-content/themes/hashone-child/searchform.php
@@ -0,0 +1,4 @@
+<!--<form role="search" method="get" id="searchform" action="--><?php //echo home_url( '/' ) ?><!--" >-->
+<!--    <label class="screen-reader-text" for="s">Поиск: </label>-->
+<!--    <input type="text" placeholder="Search..." value="--><?php //echo get_search_query() ?><!--" name="s" id="s" />-->
+<!--</form>-->
